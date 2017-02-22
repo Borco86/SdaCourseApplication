442a1f7b36ab9c059d0885e1abc3f40eab19a477
Tomek <vovovov@wp.pl>
2017-02-22 19:41:35 +0100

    6th commit

--- app/src/main/java/com/example/rent/sdacourseapplication/MainActivity.java
+++ app/src/main/java/com/example/rent/sdacourseapplication/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.rent.sdacourseapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.preference.PreferenceManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +20,7 @@ import com.example.rent.sdacourseapplication.drawing.app.DrawingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String NOTES_KEY = "notes";//przechowuje tekst
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
 
@@ -40,8 +45,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        final EditText noteEditText = (EditText) findViewById(R.id.my_note_edit_text);
+        // wczytywanie po onResume()
+        noteEditText.setText(readText());
+
+        Button saveButton = (Button) findViewById(R.id.save_note);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveText(noteEditText.getText().toString());
+            }
+        });
 
     }
+
+    //czytanie
+    private String readText() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return sharedPreferences.getString(NOTES_KEY, "");
+    }
+
+    //zapisywanie wpisywanego tekstu
+    private void saveText(String text) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.edit().putString(NOTES_KEY, text).apply();
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
